9012854324516871e9b1c64ba12a92cc30e540bc
skychx <skychx@hotmail.com>
2022-10-31 23:34:40 +0800

    feat: interpreter-1.3

--- src/lexer/lexer.go
+++ src/lexer/lexer.go
@@ -21,18 +21,10 @@ func New(input string) *Lexer {
   return l
 }
 
-func (l *Lexer) readChar() {
-  if (l.readPosition) >= len(l.input) {
-    l.ch = 0 // 0 is NULL ASCII code
-  } else {
-    l.ch = l.input[l.readPosition]
-  }
-  l.position = l.readPosition
-  l.readPosition += 1
-}
-
 func (l *Lexer) NextToken() token.Token {
   var tok token.Token
+
+  l.skipWhitespace()
 
   switch l.ch {
   case '=':
@@ -52,14 +44,73 @@ func (l *Lexer) NextToken() token.Token {
   case ';':
     tok = newToken(token.SEMICOLON, l.ch)
   case 0:
-    tok.Type = token.EOF
     tok.Literal = ""
+    tok.Type = token.EOF
+  default:
+    if isLetter(l.ch) {
+      tok.Literal = l.readIdentifier()
+      tok.Type = token.LookupIdent(tok.Literal)
+      return tok
+    }
+
+    if isDigit(l.ch) {
+      tok.Literal = l.readNumber()
+      tok.Type = token.INT
+      return tok
+    }
+
+    // unkown token or illegal token
+    tok = newToken(token.ILLEGAL, l.ch)
   }
 
   l.readChar()
   return tok
 }
 
+func (l *Lexer) readChar() {
+  if (l.readPosition) >= len(l.input) {
+    l.ch = 0 // 0 is NULL ASCII code
+  } else {
+    l.ch = l.input[l.readPosition]
+  }
+  l.position = l.readPosition
+  l.readPosition += 1
+}
+
+func (l *Lexer) readIdentifier() string {
+  position := l.position
+  for isLetter(l.ch) {
+    l.readChar()
+  }
+
+  return l.input[position:l.position]
+}
+
+func (l *Lexer) readNumber() string {
+  position := l.position
+  for isDigit(l.ch) {
+    l.readChar()
+  }
+
+  return l.input[position:l.position]
+}
+
+func (l *Lexer) skipWhitespace() {
+  for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
+    l.readChar()
+  }
+}
+
 func newToken(tokenType token.TokenType, ch byte) token.Token {
   return token.Token{Type: tokenType, Literal: string(ch)}
+}
+
+func isLetter(ch byte) bool {
+  // a-zA-Z_
+  return ('a' <= ch && ch <= 'z') || ('A' <= ch && ch <= 'Z') || ch == '_'
+}
+
+func isDigit(ch byte) bool {
+  // 0-9
+  return '0' <= ch && ch <= '9'
 }
