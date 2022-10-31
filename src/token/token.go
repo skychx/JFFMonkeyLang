fe03e89f81dc21dc8c89f83de827ab8abadaecb2
skychx <skychx@hotmail.com>
2022-10-31 23:34:40 +0800

    feat: interpreter-1.3

--- src/token/token.go
+++ src/token/token.go
@@ -3,7 +3,7 @@ package token
 type TokenType string
 
 const (
-  ILLEGAL = "ILLEGAL"
+  ILLEGAL = "ILLEGAL" // unkown, illegal token
   EOF     = "EOF"
 
   // Identifiers + literals
@@ -46,4 +46,24 @@ const (
 type Token struct {
   Type    TokenType
   Literal string
+}
+
+var keywords = map[string]TokenType{
+  "fn":     FUNCTION,
+  "let":    LET,
+  "true":   TRUE,
+  "false":  FALSE,
+  "if":     IF,
+  "else":   ELSE,
+  "return": RETURN,
+}
+
+func LookupIdent(ident string) TokenType {
+  // keyword
+  if tok, ok := keywords[ident]; ok {
+    return tok
+  }
+
+  // User Identifiers
+  return IDENT
 }
